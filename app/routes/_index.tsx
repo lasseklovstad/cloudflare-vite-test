1a56da6c6ef4b533db8fe56b725c185b14b31f0a
laklov <lasse.klovstad@gmail.com>
2024-02-08 14:10:44 +0100

    Check process env

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,15 +9,17 @@ export async function loader({ context }: LoaderFunctionArgs) {
   console.log(context)
   const { AUTH0_SECRET } = context.env;
   const value = AUTH0_SECRET
-  return json({ value });
+  const processValue = process.env
+  return json({ value, processValue });
 }
 
 export default function Index() {
-  const { value } = useLoaderData<typeof loader>();
+  const { value, processValue } = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Welcome to Remix</h1>
       {value}
+      {JSON.stringify(processValue)}
     </div>
   );
 }
